19d636713e55f192e345feb6a505321c423373df
shakhard <david.shakhar@techlib.cz>
2021-10-27 11:32:36 +0200

    formatting

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from art import logo
 import os
 
-clear = lambda: os.system('clear')
+
+def clear(): return os.system('clear')
+
 
 def add(n1, n2):
     return n1 + n2
 
+
 def substract(n1, n2):
     return n1 - n2
+
 
 def multiply(n1, n2):
     return n1 * n2
 
+
 def devide(n1, n2):
     return n1 / n2
+
 
 operations = {
     "+": add,
@@ -22,30 +28,30 @@ operations = {
     "/": devide,
 }
 
+
 def calculator():
     print(logo)
-    
+
     continue_calculating = True
     num1 = float(input("What's the first number? "))
     for symbol in operations:
         print(symbol)
-    
+
     while continue_calculating:
-        operation_symbol = input("Pick an operation: ")    
+        operation_symbol = input("Pick an operation: ")
         num2 = float(input("What's the next number? "))
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        should_continue = input(f"'y' to continue calculating with {answer}, 'n' to start new calculation: ")
+        should_continue = input(
+            f"'y' to continue calculating with {answer}, 'n' to start new calculation: ")
         if should_continue == "n":
             continue_calculating = False
             clear()
-            calculator()        
+            calculator()
         else:
             num1 = answer
 
-calculator()            
 
-
-
+calculator()
